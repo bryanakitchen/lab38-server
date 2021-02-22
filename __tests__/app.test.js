0b868865e632dedd64edbc2b182ee77c6635d8ce
BRYANA <BRYANAKITCHEN@GMAIL.COM>
2021-02-22 18:19:24 -0500

    post route

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,8 +3,26 @@ const pool = require('../lib/utils/pool');
 const request = require('supertest');
 const app = require('../lib/app');
 
-describe('lab38Server routes', () => {
+describe('Server routes', () => {
   beforeEach(() => {
-    return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'))
+    return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'));
+  });
+
+  it('Creates a flash card via POST', async() => {
+    return request(app)
+      .post('/api/v1/cards')
+      .send({
+        keyTerm: 'HTTP',
+        definition: 'Hypertext Transfer Protocol',
+        topic: 'web'
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          id: expect.any(String),
+          keyTerm: 'HTTP',
+          definition: 'Hypertext Transfer Protocol',
+          topic: 'web'
+        });
+      });
   });
 });
